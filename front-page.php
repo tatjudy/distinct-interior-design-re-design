9ef0371764e1c2c9d3e3174f699ddde2d0407db4
tatjudy <59752406+tatjudy@users.noreply.github.com>
2022-10-30 19:21:26 -0600

    One Slider

--- front-page.php
+++ front-page.php
@@ -112,51 +112,6 @@
                 endif;
             ?>
         </div> <!-- End Slider -->
-        <div class="slider">
-            <div class="slides">
-                <!--radio buttons start-->
-                <input type="radio" name="radio-btn" id="radio1">
-                <input type="radio" name="radio-btn" id="radio2">
-                <input type="radio" name="radio-btn" id="radio3">
-                <input type="radio" name="radio-btn" id="radio4">
-                <input type="radio" name="radio-btn" id="radio5">
-                <!--radio buttons end-->
-                <!--slide images start-->
-                <div class="slide first" style="background-image: url('https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/homepage-redesign/Oct22_WILDERNESS_Hero-Safe-Zones_2160x1095.jpg');">
-                    <div class="banner-text">
-                        <h1><?php echo $banner_heading_text; ?></h1>
-                        <p><?php echo $banner_paragraph_text; ?></p>
-                    </div>
-                </div>
-                <div class="slide" style="background-image: url('https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/homepage-redesign/C-Class_Hero-Safe-Zones_2160x1095_ENG.jpg');">
-                </div>
-                <div class="slide" style="background-image: url('https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/myco/my23/gla/class-page/series/MBCAN-2022-GLA-SUV-HERO-DR.jpg');">
-                </div>
-                <div class="slide" style="background-image: url('https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/myco/my23/gla/class-page/series/MBCAN-2023-GLA-SUV-CH-1-1-DR.jpg');">
-                </div>
-                <div class="slide" style="background-image: url('https://www.mercedes-benz.ca/content/dam/mb-nafta/ca/homepage-redesign/C-Class_Hero-Safe-Zones_2160x1095_ENG.jpg');">
-                </div>
-                <!--slide images end-->
-                <!--automatic navigation start-->
-                <div class="navigation-auto">
-                    <div class="auto-btn1"></div>
-                    <div class="auto-btn2"></div>
-                    <div class="auto-btn3"></div>
-                    <div class="auto-btn4"></div>
-                    <div class="auto-btn5"></div>
-                </div>
-                <!--automatic navigation end-->
-            </div>
-            <!--manual navigation start-->
-            <div class="navigation-manual">
-                <label for="radio1" class="manual-btn"></label>
-                <label for="radio2" class="manual-btn"></label>
-                <label for="radio3" class="manual-btn"></label>
-                <label for="radio4" class="manual-btn"></label>
-                <label for="radio5" class="manual-btn"></label>
-            </div>
-            <!--manual navigation end-->
-        </div>
     </section>
     <!-- <section class="carousel">
         <div class="banner-text">
